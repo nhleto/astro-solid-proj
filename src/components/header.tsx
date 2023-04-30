b04d3afb3391a0e5da91d36b57d8ef969f1d4624
henry <hnr.leto11@gmail.com>
2023-04-30 10:56:35 -0400

    add styling

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,7 @@ import type { Component } from "solid-js";
 
 const header: Component<{}> = (props) => {
   return (
-    <nav class="flex flex-row justify-between m-5">
+    <nav class="flex flex-row justify-between m-5 font-medium text-xl">
       <section>
         <span>Favicon</span>
       </section>
