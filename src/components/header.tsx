152eecf454a92e21c34e4889a5e28dc8d7efc225
henry <hnr.leto11@gmail.com>
2023-04-30 15:58:28 -0400

    styles

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,11 +2,18 @@ import type { Component } from "solid-js";
 
 const header: Component<{}> = (props) => {
   return (
-    <nav class="flex flex-row justify-between m-5 font-medium text-xl">
-      <section>
-        <span>Favicon</span>
+    <nav class="max-sm:flex-col max-sm:items-center flex flex-row justify-between m-5 font-medium text-xl">
+      <section class="flex flex-col max-sm:my-3 max-sm:items-center">
+        <span class="text-5xl">Henry Leto</span>
+        <span class="text-xl">Full Stack Developer</span>
       </section>
-      <section>Text</section>
+      <section class="flex flex-row gap-3">
+        <span>About</span>
+        <span>Experience</span>
+        <span>Projects</span>
+        <span>Contact</span>
+        <span>Github</span>
+      </section>
     </nav>
   );
 };
